31368406ebcbc5478c2a55c21a547a5bb283d598
paulplayer <paulplayergg@gmail.com>
2018-11-30 16:20:38 +0800

    temp save

--- src/core/coin/huobi.py
+++ src/core/coin/huobi.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+
+# Huobi Class
+
+from src.core.coin.lib.huobipro_api.Utils import ACCESS_KEY
+from src.core.coin.coin import Coin
+from src.core.util.exceptions import HuobiException
+from src.core.util.helper import date_to_milliseconds, interval_to_milliseconds
+
+class Huobi(Coin):
+
+    def __init__(self, exchange, api_key, api_secret, passphrase,
+                 proxies=None):
+        super(Huobi, self).__init__(exchange, api_key, api_secret, proxies)
