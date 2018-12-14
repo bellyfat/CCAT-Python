5179111bfbf382f88386b7457e27d7dce26229af
paulplayer <paulplayergg@gmail.com>
2018-12-14 16:42:40 +0800

    trans

--- src/core/router.py
+++ src/core/router.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from multiprocessing import Pipe
-
-from src.core.util.exceptions import RouterException
+from src.core.engine.engine import EventEngine
+from src.core.engine.handler import Handler
+from src.core.engine.register import Register
+from src.core.engine.sender import Sender
+from src.core.util.exceptions import RouterException, UtilException
 from src.core.util.log import Logger
+from src.core.util.util import Util
 
 
 class Router(object):
     def __init__(self):
-        pass
+        self._logger = Logger()
+
+    def initAPP(self):
